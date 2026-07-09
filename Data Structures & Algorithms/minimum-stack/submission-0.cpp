373b3cab816a55b4ce4f9b7984d91b1db1eb5835
Ujjwal Kalra <ujj.kalra10@gmail.com>
2026-07-09 13:45:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<pair<int,int>> s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s.empty()){
+            s.push({val,val});
+        }
+        else{
+int minVal = min(val,s.top().second);
+s.push({val,minVal});
+        }
+        
+    }
+    
+    void pop() {
+        s.pop();
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+};
